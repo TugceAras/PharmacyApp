6d9125d1a22c7b9955e00859cf90409676dae5d9
TugceAras <aras.tugcee@gmail.com>
2023-08-14 21:41:11 +0300

    gradle file updated

--- app/src/main/java/com/tugcearas/pharmacyapp/util/constants/Constants.kt
+++ app/src/main/java/com/tugcearas/pharmacyapp/util/constants/Constants.kt
@@ -1,9 +1,10 @@
 package com.tugcearas.pharmacyapp.util.constants
 
+import com.tugcearas.pharmacyapp.BuildConfig
+
 object Constants {
 
     const val BASE_URL = "https://www.nosyapi.com/"
-    const val API = "tDmNAG3S4pNTrp9EeiaBolTS2grCSazCFptcKNfNcOdkj8UiXX678ewEM9Am"
-    const val GET_LIST = "apiv2/pharmacyLink?city=istanbul&county=umraniye&apikey=$API"
+    const val GET_LIST = "apiv2/pharmacyLink?city=istanbul&county=umraniye&apikey=${BuildConfig.API_KEY}"
     const val APP_GALLERY_PROJECT_API = "DAEDAFGgnviLqr3jHW/GIDD8LwJQpNqTnrnBB20Nuct9UW4rGgV4LE8ATIECc93Lb4LABT9fAWSBkr4Q39ZOIAJZOPWH49heH0Jwhw=="
 }
